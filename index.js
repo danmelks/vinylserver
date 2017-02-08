4302cb42aa60366de1044a6b0a1395f24146ae4b
Andrew F. Davis <glneolistmail@gmail.com>
2017-02-08 05:34:36 +0100

    Cleanup source and add real print command
    Signed-off-by: Andrew F. Davis <glneolistmail@gmail.com>

--- index.js
+++ index.js
@@ -1,64 +1,41 @@
-//const sys = require('sys');
 const express = require('express');
 const fileUpload = require('express-fileupload');
 const app = express();
 const exec = require('child_process').exec;
 
-app.use(express.static('public'))
-//app.use(bodyParser());
+app.use(express.static('public'));
 
-/*
-app.get('/', function (req, res) {
-  res.send('Hello World!')
-})
-*/
-
-
-
-
-
-let child;
-
+var child;
  
-// default options 
 app.use(fileUpload());
  
 app.post('/upload', function(req, res) {
-  let sampleFile;
- 
-  if (!req.files) {
-    res.send('No files were uploaded.');
-    return;
-  }
- 
-  // The name of the input field (i.e. "sampleFile") is used to retrieve the uploaded file 
-  sampleFile = req.files.sampleFile;
- 
-  // Use the mv() method to place the file somewhere on your server 
-  sampleFile.mv('public/myfile.hpgl', function(err) {
-    if (err) {
-      res.status(500).send(err);
-    } else {
-      console.log('code for script goes here')
-      
+	var sampleFile;
 
-      res.send('File uploaded!');
+	if (!req.files || !req.files.sampleFile) {
+		res.send('No files were uploaded.');
+		return;
+	}
 
-            // executes `pwd`
-      child = exec("mysomoscript", function (error, stdout, stderr) {
-        //sys.print('stdout: ' + stdout);
-        //sys.print('stderr: ' + stderr);
-        if (error !== null) {
-          console.log('exec error: ' + error);
-        } else {
-          console.log('exec stdout: ' + stdout);
-        }
-      });
-    }
-  });
+	sampleFile = req.files.sampleFile;
+
+	sampleFile.mv('public/myfile.hpgl', function(err) {
+		if (err) {
+			res.status(500).send(err);
+		} else {
+			res.send('File uploaded!');
+
+			child = exec("stty 9600 ixon ixoff -F /dev/ttyUSB0; cat public/myfile.hpgl > /dev/ttyUSB0", function (error, stdout, stderr) {
+				if (error !== null)
+					console.log('exec error: ' + error);
+				else
+					console.log('exec stdout: ' + stdout);
+			});
+		}
+	});
 });
 
 
-app.listen(3000, function () {
-  console.log('Example app listening on port 3000!')
-})
+app.listen(80, function () {
+	console.log('Vinyl server listening on port 80!')
+});
